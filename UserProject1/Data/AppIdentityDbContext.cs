d50d8841829630513b9d7429e353fa43e9a488fb
Kaktus200020 <artemkremnev2007@gmail.com>
2024-03-30 19:21:20 +0300

    Init

---
+++ UserProject1/Data/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserProject1.Models;
+
+namespace UserProject1.Data
+{
+    public class AppIdentityDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) { }
+    }
+}
