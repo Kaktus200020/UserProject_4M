b0d809bdb03d85e57f7e1c001a2649cf613fd5a3
Kaktus200020 <artemkremnev2007@gmail.com>
2024-03-30 20:01:49 +0300

    Added Admin Controller v1

---
+++ UserProject1/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserProject1.Models;
+
+namespace UserProject1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
